66c686d2ae1e23668ad5445a76aa6d9f85950a37
Ryan <ryguyrobertson@gmail.com>
2024-11-27 14:18:53 -0800

    add script messing around

---
+++ scenes/logo.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+var test_scale: int = 1
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#test_scale = 2
+	#scale = Vector2(test_scale, test_scale)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#test_scale += 1
+	#scale = Vector2(test_scale, test_scale)
+	pass
