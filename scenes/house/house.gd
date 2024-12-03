d2e54796b3b5332d777778f66cdb94da0cc1dc90
Ryan <ryguyrobertson@gmail.com>
2024-12-03 13:30:04 -0800

    update house, add player zoom in/out w/ entry/exit

---
+++ scenes/house/house.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+signal player_entered
+signal player_exited
+
+func _on_body_entered(body: Node2D) -> void:
+	player_entered.emit()
+	
+func _on_body_exited(body: Node2D) -> void:
+	player_exited.emit()
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -21,3 +21,13 @@ func _on_player_grenade(pos, direction) -> void:
 func _on_gate_player_entered_gate(body: Variant) -> void:
 	print("entered gate at level")
 	print(body)
+
+
+func _on_house_player_entered() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1)
+
+
+func _on_house_player_exited() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 1)
